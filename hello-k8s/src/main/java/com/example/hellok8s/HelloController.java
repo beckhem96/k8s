709adc34b997076321f123a113497f1883a8b495
beckhem96 <beckhem96@naver.com>
2026-04-17 17:36:18 +0900

    first commit & hello k8s

---
+++ hello-k8s/src/main/java/com/example/hellok8s/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.hellok8s;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Kubernetes! This is a Spring Boot App";
+    }
+}
